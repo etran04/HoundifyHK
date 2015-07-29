92dbb190c32522ad5181debca00c5f5d0fac926b
Eric Tan <Eric@SparkPR-Image.local>
2015-07-29 14:00:09 -0700

    Initial Commit

---
+++ HoundifyHK/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HoundifyHK
+//
+//  Created by Eric Tan on 7/29/15.
+//  Copyright (c) 2015 Harman International. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
